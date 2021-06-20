c98bfdacaf5ae3becc672d70fa29cf0c84ca32ac
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-20 17:02:27 +0200

    Add static version of edit page and create API endpoint to save data into KVdb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [repoData, setRepoData] = useState({});
+  const [repoData, setRepoData] = useState(dummyData);
   return (
     <Component {...pageProps} repoData={repoData} setRepoData={setRepoData} />
   );
 }
 export default MyApp;
+
+const dummyData = {
+  url: "https://github.com/vercel/next.js",
+  ownerName: "vercel",
+  repoName: "next.js",
+  description: "The React Framework",
+  avatarUrl: "https://avatars.githubusercontent.com/u/14985020?v=4",
+  language: "JavaScript",
+  stars: 69601,
+  watchers: 1218,
+  forks: 13075,
+};
--- pages/edit/index.tsx
+++ pages/edit/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
 
 interface Props {
@@ -5,6 +8,7 @@ interface Props {
 }
 
 const Index: React.FC<Props> = ({ repoData }) => {
+  const [showStats, setShowStats] = useState(true);
   const {
     url,
     ownerName,
@@ -16,15 +20,56 @@ const Index: React.FC<Props> = ({ repoData }) => {
     watchers,
     forks,
   } = repoData;
-  return (
-    <div>
-      <h1>{repoName}</h1>
-      <p>{description}</p>
+
+  const handleSubmit = () => {};
+
+  if (Object.keys(repoData).length === 0) {
+    return (
       <div>
-        <Image src={avatarUrl} height={144} width={144} alt={ownerName} />
+        <h1>Empty Page</h1>
+        <Link href="/">
+          <a>Go back to homepage</a>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h1>{repoName}</h1>
+          <p>{description}</p>
+          <div>
+            <Image src={avatarUrl} height={144} width={144} alt={ownerName} />
+          </div>
+        </div>
+        <form>
+          <label htmlFor="pickIcon">Pick Icon</label>
+          <input type="text" name="pickIcon" id="pickIcon" />
+          <br />
+          <label htmlFor="color">Pick Color</label>
+          <input type="color" name="color" id="color" />
+          <br />
+          <label htmlFor="showStats">Show Stats</label>
+          <input type="checkbox" name="showStats" id="showStats" />
+          <br />
+          <label htmlFor="showLanguage">Show Language</label>
+          <input type="checkbox" name="showLanguage" id="showLanguage" />
+          <br />
+          <label htmlFor="showTopContributors">Show op Contributors</label>
+          <input
+            type="checkbox"
+            name="showTopContributors"
+            id="showTopContributors"
+          />
+          <br />
+          <label htmlFor="fontStyle">Pick Font-Style</label>
+          <input type="text" name="fontStyle" id="fontStyle" />
+          <br />
+          <button type="submit">Submit and Share</button>
+        </form>
+      </>
+    );
+  }
 };
 
 export default Index;
