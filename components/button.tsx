5fd098fe12760771284ed0265a3d11f9e34fe08b
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-21 16:42:17 +0200

    Add onClick functionality to button component

--- components/button.tsx
+++ components/button.tsx
@@ -4,13 +4,20 @@ interface Props {
   children: React.ReactNode;
   color: string;
   type?: "button" | "reset" | "submit";
+  onClick: () => void;
 }
 
-const Button: React.FC<Props> = ({ children, color, type = "button" }) => {
+const Button: React.FC<Props> = ({
+  children,
+  color,
+  type = "button",
+  onClick,
+}) => {
   return (
     <button
       className={`mt-6 py-2 block w-full transition-colors duration-150 bg-${color}-500 hover:bg-${color}-400 text-white rounded-lg focus:outline-none`}
       type={type}
+      onClick={onClick}
     >
       {children}
     </button>
