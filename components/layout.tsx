541b51c5224f471b5e2045aeac1103d66272aabc
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-21 22:46:00 +0200

    Add custom icon and logo

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
   children: React.ReactNode;
@@ -14,11 +15,17 @@ const Layout: React.FC<Props> = ({ children }) => {
           name="description"
           content="My submission for the superchat-frontend-challenge"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" />
       </Head>
       <header className="m-5">
         <Link href="/">
-          <a className="text-2xl text-white">Superrepos</a>
+          <Image
+            className="cursor-pointer"
+            src="/logo-white.svg"
+            width="200"
+            height="38"
+            alt="Logo"
+          ></Image>
         </Link>
       </header>
       <main className="mx-5 mt-10 flex justify-center">
