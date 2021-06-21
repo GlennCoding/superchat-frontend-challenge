b38e8b22d58e1825d700ea97fa217b7f93186178
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-21 12:17:15 +0200

    Refactor API Route

--- pages/api/repos/[id].tsx
+++ pages/api/repos/[id].tsx
@@ -5,15 +5,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const bucket = KVdb.bucket("TxcuiQGRNy6E2JGa6RmfG3");
   const id = req.query.id;
-  // const id = "abc";
 
   let data;
   try {
     data = await bucket.get(id);
-    res.status(200).json(data);
   } catch {
-    res.status(404).json({ message: "Not Found" });
+    return res.status(404).json({ message: "Not Found" });
   }
+  return res.status(200).json(data);
 };
 
 export default handler;
