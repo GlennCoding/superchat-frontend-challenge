2023b96407337ceaa82d57d6b2cc094258df41b4
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-21 16:16:23 +0200

    Replace JSX with components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import router, { useRouter } from "next/router";
-import Image from "next/image";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import { getRepoData } from "../lib/repos";
-import { route } from "next/dist/next-server/server/router";
+import Layout from "../components/layout";
+import Button from "../components/button";
+import Input from "../components/input";
 
 interface Props {
   setRepoData: any;
@@ -28,57 +29,28 @@ const Home: React.FC<Props> = ({ setRepoData }) => {
     router.push("./create");
   };
   return (
-    <div>
-      <Head>
-        <title>Superrepos</title>
-        <meta
-          name="description"
-          content="My submission for the superchat-frontend-challenge"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <nav className="ml-5 mt-5">
-        <p className="text-2xl text-white">Superrepos</p>
-      </nav>
-      <main className="mx-5 mt-10 flex justify-center">
-        <div className="px-8 py-10 w-full md:max-w-screen-sm bg-white rounded-2xl shadow-lg">
-          <h2 className="text-xl text-center font-semibold mb-4">
-            Create a superrepo
-          </h2>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col mb-2">
-              <label htmlFor="user">User</label>
-              <input
-                className="border py-2 px-3 text-grey-darkest rounded-lg"
-                type="text"
-                value={user}
-                onChange={(e) => setUser(e.target.value)}
-                name="user"
-                id="user"
-              />
-            </div>
-            <div className="flex flex-col mb-2">
-              <label htmlFor="repo">Repository</label>
-              <input
-                className="border py-2 px-3 text-grey-darkest rounded-lg"
-                type="text"
-                value={repo}
-                onChange={(e) => setRepo(e.target.value)}
-                name="repo"
-                id="repo"
-              />
-            </div>
-            <button
-              className="mt-6 py-2 block w-full transition-colors duration-150 bg-green-500 hover:bg-green-400 text-white rounded-lg focus:outline-none"
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
+    <Layout>
+      <h2 className="text-xl text-center font-semibold mb-4">
+        Create a superrepo
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label>
+            User
+            <Input value={user} onChange={(e) => setUser(e.target.value)} />
+          </label>
         </div>
-      </main>
-    </div>
+        <div className="mb-2">
+          <label>
+            Repo
+            <Input value={repo} onChange={(e) => setRepo(e.target.value)} />
+          </label>
+        </div>
+        <Button color="green" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Layout>
   );
 };
 
