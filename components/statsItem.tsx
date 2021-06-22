b2dbaa1a3d7d12704ac28eba1716a3439832c9f9
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-22 13:16:18 +0200

    Improve CSS classes

--- components/statsItem.tsx
+++ components/statsItem.tsx
@@ -8,7 +8,7 @@ const StatsItem: React.FC<Props> = ({ children, color, url }) => {
   return (
     <a href={url} className="flex justify-center">
       <div
-        className={`w-60 sm:w-40 py-2 bg-${color}-300 rounded-md mb-4 shadow-md hover:bg-${color}-400 mx-2`}
+        className={`w-full max-w-60 sm:w-40 py-2 bg-${color}-300 rounded-md mb-4 shadow-md hover:bg-${color}-400 mx-2`}
       >
         {children}
       </div>
