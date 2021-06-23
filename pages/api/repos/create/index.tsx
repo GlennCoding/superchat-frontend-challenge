e627ad412586540e691c7f6de5229f766fba7e4e
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-23 15:06:19 +0200

    Cleanup try catch block

--- pages/api/repos/create/index.tsx
+++ pages/api/repos/create/index.tsx
@@ -7,19 +7,22 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const bucket = KVdb.bucket("TxcuiQGRNy6E2JGa6RmfG3");
   const data = req.body;
 
-  while (true) {
-    const newId = uuidv4().slice(0, 7);
+  let newId: string | undefined;
+
+  let uniqueIdGenerated = false;
+  while (!uniqueIdGenerated) {
+    newId = uuidv4().slice(0, 7);
     try {
       await bucket.get(newId);
     } catch {
-      try {
-        await bucket.set(newId, JSON.stringify(data));
-        false;
-        return res.status(200).json({ repoId: newId });
-      } catch {
-        return res.status(500);
-      }
+      uniqueIdGenerated = true;
     }
+  }
+  try {
+    await bucket.set(newId, JSON.stringify(data));
+    return res.status(200).json({ repoId: newId });
+  } catch (err) {
+    return res.status(500).json({ message: err });
   }
 };
 
